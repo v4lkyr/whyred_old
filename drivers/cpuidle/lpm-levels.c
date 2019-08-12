33d95ad373543ccf2277f610eaa015ee1537bf5e
v4lkyr <valkyr23@gmail.com>
2019-08-12 11:33:17 +0800

    cpuidle: lpm-levels: Add back clock_debug_print_enabled
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1761,6 +1761,14 @@ static int lpm_suspend_enter(suspend_state_t state)
 	cpu_prepare(cluster, idx, false);
 	cluster_prepare(cluster, cpumask, idx, false, 0);
 
+	/*
+	* Print the clocks which are enabled during system suspend
+	* This debug information is useful to know which are the
+	* clocks that are enabled and preventing the system level
+	* LPMs(XO and Vmin).
+	*/
+	clock_debug_print_enabled();
+
 	BUG_ON(!use_psci);
 	psci_enter_sleep(cluster, idx, true);
 
@@ -1780,7 +1788,6 @@ static int lpm_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct kobject *module_kobj = NULL;
-	struct md_region md_entry;
 
 	get_online_cpus();
 	lpm_root_node = lpm_of_parse_cluster(pdev);
