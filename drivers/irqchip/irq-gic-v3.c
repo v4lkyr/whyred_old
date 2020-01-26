a979fa88043efcad38da3325432d517d38a6aa85
v4lkyr <valkyr23@gmail.com>
2020-01-27 06:39:41 +0800

    irq: Add wakeup reason logging
    log wakeup reason for irq
    
    Bug: 28852315
    Change-Id: Iad9cf7423e51441c81f52fd94dd6347de6709262
    Signed-off-by: Wei Wang <wvw@google.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -26,6 +26,7 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/wakeup_reason.h>
 
 #include <linux/irqchip.h>
 #include <linux/irqchip/arm-gic-v3.h>
@@ -445,22 +446,8 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
+		log_base_wakeup_reason(irq);
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
-
-		/*Add-begin-HMI_M6100_A01-60
-		**Comment:Logging kernel wakeup reson
-		*/
-
-		/* irq 75 :rpm interrupt
-		** irq 12 :spmi interrupt
-		** irq 199:pinctrl interrupt
-		*/
-		if (75 == irq || 12 == irq || 199 == irq)
-			continue;
-
-		log_wakeup_reason(irq);
-
-		/*Add-end HMI_M6100_A01-60*/
 
 	}
 }
